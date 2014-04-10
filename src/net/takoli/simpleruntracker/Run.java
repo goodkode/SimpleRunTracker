b9606968f26fdda9eb90dc16f473f3f9de8e1a2a
Oliver <takacsoliver@hotmail.com>
2014-04-10 17:25:39 -0400

    Update Run.java

--- src/net/takoli/simpleruntracker/Run.java
+++ src/net/takoli/simpleruntracker/Run.java
@@ -18,4 +18,33 @@ public class Run {
 			expanded = false;
 		else expanded = true;
 	}
+	
+	public static class Time {
+		private h, m, s;
+		public Time(int hour, int min, int sec) {
+			h = hour;
+			m = min;
+			s = sec;
+		}
+		
+	}
+	
+	public static class Distance {
+		private char unit;
+		private float dist;
+		private final float CONV = 1.609344f  // 1 mile in km
+		
+		public Distance(float distance, char unit) {
+			dist = distance;
+			this.unit = unit;
+		}
+		
+		public void convertM2Km() {
+			
+		}
+		
+		public void convertKm2M() {
+			
+		}
+	}
 }
