d1912791f3c3548c2042ba7222e40e4e33be6d29
Oliver <takacsoliver@hotmail.com>
2014-04-16 15:40:45 -0400

    Update Run.java

--- src/net/takoli/simpleruntracker/Run.java
+++ src/net/takoli/simpleruntracker/Run.java
@@ -15,6 +15,20 @@ public class Run {
 		expanded = false;
 	}
 	
+	public Run(String line) {
+		String[] fields = line.split(",");
+		Log.i("run", fields[0] +fields[1] + fields[2]);
+		String[] distSt = fields[0].split("\\.");
+		String[] timeSt = fields[2].split("\\:");
+		this.dd = Integer.parseInt(distSt[0]);
+		this._dd = Integer.parseInt(distSt[1]);
+		this.unit = fields[1];
+		this.h = Integer.parseInt(timeSt[0]);
+		this.mm = Integer.parseInt(timeSt[1];
+		this.ss = Integer.parseInt(timeSt[2];
+		expanded = false;
+	}
+	
 	public String getDistance() {
 		return dd + "." + _dd;
 	}
@@ -31,5 +45,9 @@ public class Run {
 	public void switchDetails() {
 		if (expanded)   expanded = false;
 		else expanded = true;
+	}
+	
+	public String toString() {
+		return dd+"."+_dd+","+unit+","+h+":"+mm+":"+ss;
 	}
 }
