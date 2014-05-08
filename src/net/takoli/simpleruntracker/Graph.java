10c3d357283d97b2a975be5b660d91423cef0549
Oliver <takacsoliver@hotmail.com>
2014-05-08 17:28:52 -0400

    Update Graph.java

--- src/net/takoli/simpleruntracker/Graph.java
+++ src/net/takoli/simpleruntracker/Graph.java
@@ -2,11 +2,37 @@ package net.takoli.simpleruntracker;
 
 public class Graph {
 	
-	public Graph() {
+	private ArrayList<Run> graphRunList;
+	private int fullSize, plotSize;
+	private final int MAX_PLOTS = 10;
+	
+	// nothing is really needed here
+	public Graph() { }
+	
+	public void update(RunDB runListDB) {
+		readInGraphRuns(runListDB);
+		measureViewAndGraph();
+		drawPlots();
+	}
+	
+	// Utility for drawing:
+	private void readInGraphRuns(RunDB runListDB) {
+		ArrayList<Run> runList = runListDB.getRunList();
+		this.fullSize = runList.size();
+		if (fullSize > MAX_PLOTS)
+			plotSize = MAX_PLOTS;
+		else
+			plotSize = fullSize;
+		graphRunList = new ArrayList<Run>();
+		for (int i = fullSize - 1; i >= fullSize - plotSize; i--)
+			graphRunList.add(runList.get(i);
+	}
+	
+	private void measureViewAndGraph() {
 		
 	}
 	
-	public void update() {
+	private void drawPlots() {
 		
 	}
 }
