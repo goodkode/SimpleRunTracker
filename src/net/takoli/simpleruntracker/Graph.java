e1b028f2335779cf86b0ed57d49f68001eb21530
Oliver <takacsoliver@hotmail.com>
2014-05-09 15:44:33 -0400

    Update Graph.java

--- src/net/takoli/simpleruntracker/Graph.java
+++ src/net/takoli/simpleruntracker/Graph.java
@@ -1,18 +1,40 @@
 package net.takoli.simpleruntracker;
 
-public class Graph {
+public class GraphView extends View {
 	
-	private ArrayList<Run> graphRunList;
-	private int fullSize, plotSize;
+	private ArrayList<Run> runList;
 	private final int MAX_PLOTS = 10;
+	private int plotSize;
+	private float[] dists, speeds;
+	private float distMin, distMax, speedMin, speedMax;
+	private boolean km;
 	
-	// nothing is really needed here
-	public Graph() { }
+	// set up the view
+	public GraphView(Context context, AttributeSet attrs) {
+	        super(context, attrs);
+	        // initialize fields
+	        runList = ((MainActivity) getActivity()).getRunDB().getRunList();
+	        dists = new float[MAX_PLOTS];
+	        speeds = new float[MAX_PLOTS];
+	        km = 
+	        refreshData();
+	}
 	
-	public void update(RunDB runListDB) {
-		readInGraphRuns(runListDB);
-		measureViewAndGraph();
-		drawPlots();
+	public void refreshData() {
+		int fullSize = runList.size();
+		plotSize = fullSize > MAX_PLOTS ? MAX_PLOTS : fullSize;
+		distMin = speedMin = 0f;
+		distMax = speedMax = 0f;
+		for (int i = fullSize - plotSize; i < fullSize; i++) {
+			dists[i] = runList.get(i).
+			speeds[i] = runList.get(i).
+		}
+	}
+	
+	@Override
+    	protected void onDraw(Canvas canvas) {
+	        drawCoordinates(canvas, distMin, distMax, speedMin, speedMax);
+	        drawChart(canvas);
 	}
 	
 	// Utility for drawing:
@@ -28,11 +50,4 @@ public class Graph {
 			graphRunList.add(runList.get(i);
 	}
 	
-	private void measureViewAndGraph() {
-		
-	}
-	
-	private void drawPlots() {
-		
-	}
 }
